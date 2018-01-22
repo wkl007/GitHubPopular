049cbcb452aea3aef79b988361653298991c28fa
wkl007 <18404969231@163.com>
2018-01-22 22:01:42 +0800

    关于页面完成

---
+++ src/page/about/AboutCommon.js
@@ -0,0 +1,177 @@
+'use strict';
+import React, {Component} from 'react';
+import {
+  Dimensions,
+  Image,
+  ListView,
+  Platform,
+  PixelRatio,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import ParallaxScrollView from 'react-native-parallax-scroll-view';
+import GlobalStyles from '../../assets/styles/GlobalStyles'
+import config from '../../assets/data/config'
+import ViewUtils from '../../util/ViewUtils'
+import FavoriteDao from '../../expand/dao/FavoriteDao'
+
+export let FLAG_ABOUT = {flag_about: 'about', flag_about_me: 'about_me'};
+
+export default class AboutCommon {
+  constructor(props, updateState, flag_about, config) {
+    this.props = props;
+    this.updateState = updateState;
+    this.flag_about = flag_about;
+    this.config = config;
+    this.favoriteDao = new FavoriteDao(FLAG_ABOUT.flag_popular);
+    this.repositories = [];
+
+    this.favoriteKeys=null;
+  }
+
+  getParallaxRenderConfig(params) {
+    let config = {};
+    config.renderBackground = () => (
+      <View key="background">
+        <Image source={{
+          uri: params.backgroundImg,
+          width: window.width,
+          height: PARALLAX_HEADER_HEIGHT
+        }}/>
+        <View style={{
+          position: 'absolute',
+          top: 0,
+          width: window.width,
+          backgroundColor: 'rgba(0,0,0,.4)',
+          height: PARALLAX_HEADER_HEIGHT
+        }}/>
+      </View>
+    );
+    config.renderForeground = () => (
+      <View key="parallax-header" style={styles.parallaxHeader}>
+        <Image style={styles.avatar} source={{
+          uri: params.avatar,
+          width: AVATAR_SIZE,
+          height: AVATAR_SIZE
+        }}/>
+        <Text style={styles.sectionSpeakerText}>
+          {params.name}
+        </Text>
+        <Text style={styles.sectionTitleText}>
+          {params.description}
+        </Text>
+      </View>
+    );
+    config.renderStickyHeader = () => (
+      <View key="sticky-header" style={styles.stickySection}>
+        <Text style={styles.stickySectionText}>{params.name}</Text>
+      </View>
+    );
+    config.renderFixedHeader = () => (
+      <View key="fixed-header" style={styles.fixedSection}>
+        {ViewUtils.getLeftButton(() => {
+          this.props.navigation.goBack();
+        })}
+      </View>
+    );
+    return config;
+  }
+
+  render(contentView, params)  {
+    let renderConfig = this.getParallaxRenderConfig(params);
+    return (
+      <ParallaxScrollView
+        headerBackgroundColor="#333"
+        backgroundColor='#2196f3'
+        stickyHeaderHeight={STICKY_HEADER_HEIGHT}
+        parallaxHeaderHeight={PARALLAX_HEADER_HEIGHT}
+        backgroundSpeed={10}
+        {...renderConfig}
+      >
+        {contentView}
+      </ParallaxScrollView>
+    );
+  }
+}
+
+const window = Dimensions.get('window');
+
+const AVATAR_SIZE = 120;
+const ROW_HEIGHT = 60;
+const PARALLAX_HEADER_HEIGHT = 350;
+const STICKY_HEADER_HEIGHT = 70;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black'
+  },
+  background: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: window.width,
+    height: PARALLAX_HEADER_HEIGHT
+  },
+  stickySection: {
+    height: STICKY_HEADER_HEIGHT,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: (Platform.OS == 'ios') ? 20 : 0,
+  },
+  stickySectionText: {
+    color: 'white',
+    fontSize: 20,
+    margin: 10
+  },
+  fixedSection: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    top: 0,
+    paddingRight: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: (Platform.OS == 'ios') ? 20 : 0,
+    justifyContent: 'space-between'
+  },
+  fixedSectionText: {
+    color: '#999',
+    fontSize: 20
+  },
+  parallaxHeader: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: 100
+  },
+  avatar: {
+    marginBottom: 10,
+    borderRadius: AVATAR_SIZE / 2
+  },
+  sectionSpeakerText: {
+    color: 'white',
+    fontSize: 24,
+    paddingVertical: 5
+  },
+  sectionTitleText: {
+    color: 'white',
+    fontSize: 18,
+    paddingVertical: 5
+  },
+  row: {
+    overflow: 'hidden',
+    paddingHorizontal: 10,
+    height: ROW_HEIGHT,
+    backgroundColor: 'white',
+    borderColor: '#ccc',
+    borderBottomWidth: 1,
+    justifyContent: 'center'
+  },
+  rowText: {
+    fontSize: 20
+  }
+});
--- src/page/about/AboutPage.js
+++ src/page/about/AboutPage.js
@@ -17,25 +17,81 @@ import {MORE_MENU} from "../../common/MoreMenu";
 import GlobalStyles from '../../assets/styles/GlobalStyles'
 import ViewUtils from '../../util/ViewUtils'
 import config from '../../assets/data/config'
+import ParallaxScrollView from 'react-native-parallax-scroll-view';
+import {FLAG_LANGUAGE} from "../../expand/dao/LanguageDao";
+import AboutCommon, {FLAG_ABOUT} from "./AboutCommon";
 
 export default class AboutPage extends Component {
   constructor(props) {
     super(props);
+    this.aboutCommon = new AboutCommon(props, (dic) => {
+      this.updateState(dic)
+    }, FLAG_ABOUT.flag_about, config);
     this.state = {
       projectModels: [],
       author: config.author
     }
   }
 
+ /* componentDidMount() {
+    this.aboutCommon.componentDidMount();
+  }
+
+  componentWillunmount() {
+    this.aboutCommon.componentWillUnmount();
+  }*/
+
+  updateState(dic) {
+    this.setState(dic);
+  }
+
+  onClick(tab) {
+    let TargetComponent, parame = {menuType: tab};
+    switch (tab) {
+      case MORE_MENU.About_Author:
+        TargetComponent = 'CustomKeyPage';
+        parame.flag = FLAG_LANGUAGE.flag_language;
+        break;
+      case MORE_MENU.Website:
+        TargetComponent = 'CustomKeyPage';
+        parame.flag = FLAG_LANGUAGE.flag_key;
+        break;
+      case MORE_MENU.Feedback:
+        TargetComponent = 'CustomKeyPage';
+        parame.flag = FLAG_LANGUAGE.flag_key;
+        parame.isRemoveKey = true;
+        break;
+    }
+    if (TargetComponent) {
+      // this.props.navigation.navigate(TargetComponent, parame)
+    }
+  }
+
+
   render() {
     let content = <View>
-      关于
+      {ViewUtils.getSettingItem(() => {
+          this.onClick(MORE_MENU.Website)
+        }, require('../../assets/images/ic_computer.png'), MORE_MENU.Website, {tintColor: '#2196f3'}
+      )}
+      <View style={GlobalStyles.line}/>
+      {ViewUtils.getSettingItem(() => {
+          this.onClick(MORE_MENU.About_Author)
+        }, require('../my/images/ic_insert_emoticon.png'), MORE_MENU.About_Author, {tintColor: '#2196f3'}
+      )}
+      <View style={GlobalStyles.line}/>
+      {ViewUtils.getSettingItem(() => {
+          this.onClick(MORE_MENU.Feedback)
+        }, require('../../assets/images/ic_feedback.png'), MORE_MENU.Feedback, {tintColor: '#2196f3'}
+      )}
     </View>;
-
     return (
-      {content}
+      this.aboutCommon.render(content, {
+        name: 'GitHub Popular',
+        description: '这是一个用来查看GitHub最受欢迎与最热项目的App,它基于React Native支持Android和iOS双平台。',
+        avatar: this.state.author.avatar1,
+        backgroundImg: this.state.author.backgroundImg1
+      })
     )
   }
 }
-
-
