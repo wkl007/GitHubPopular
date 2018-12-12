34df64614027474e299bcde22308940aeab25980
wkl--007 <499657357@qq.com>
2018-12-12 21:45:44 +0800

    项目升级

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import { AppRegistry } from 'react-native'
-import App from './src/page/Setup'
+/** @format */
 
-AppRegistry.registerComponent('GitHubPopular', () => App)
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
--- src/assets/styles/GlobalStyles.js
+++ src/assets/styles/GlobalStyles.js
@@ -1,24 +1,23 @@
 /**
  * 全局样式
- * @flow
  */
-import {
-  Dimensions
-} from 'react-native'
 
-const {height, width} = Dimensions.get('window')
-module.exports = {
-  line: {
-    height: 0.4,
-    opacity: 0.5,
-    backgroundColor: 'darkgray',
-  },
-  root_container: {
-    flex: 1,
-    backgroundColor: '#f3f3f4',
-  },
-  backgroundColor: '#f3f3f4',
-  nav_bar_height_ios: 44,
-  nav_bar_height_android: 50,
-  window_height: height,
+import {Dimensions} from "react-native";
+
+const BACKGROUND_COLOR = '#f3f3f4';
+const {height, width} = Dimensions.get('window');
+export default {
+    line: {
+        height: 0.5,
+        opacity: 0.5,
+        backgroundColor: 'darkgray',
+    },
+    root_container: {
+        flex: 1,
+        backgroundColor: BACKGROUND_COLOR
+    },
+    nav_bar_height_ios: 44,
+    nav_bar_height_android: 50,
+    backgroundColor: BACKGROUND_COLOR,
+    window_height: height,
 }
