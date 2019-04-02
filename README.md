274a9aa74f8c7db59b68a9b089d37aeb30934123
wkl007 <499657357@qq.com>
2019-04-02 17:53:07 +0800

    添加友盟统计

--- README.md
+++ README.md
@@ -30,15 +30,17 @@
 ## 插件使用
 
 * [GitHubTrending ^3.0.0](https://github.com/crazycodeboy/GitHubTrending)
-* [react-navigation ^2.3.1](https://reactnavigation.org/)
-* [react-native-tab-navigator ^0.3.4](https://github.com/happypancake/react-native-tab-navigator)
+* [react-navigation ^3.6.0](https://reactnavigation.org/)
+* [react-native-vector-icons ^6.4.2](https://github.com/oblador/react-native-vector-icons)
+* [react-native-webview ^5.5.0](https://github.com/react-native-community/react-native-webview)
+* [react-navigation-tabs ^2.1.0](https://github.com/react-navigation/react-navigation-tabs)
 * [react-native-scrollable-tab-view ^0.8.0](https://github.com/skv-headless/react-native-scrollable-tab-view)
-* [react-native-easy-toast ^1.1.0](https://github.com/crazycodeboy/react-native-easy-toast)
-* [react-native-check-box ^2.1.0](https://github.com/crazycodeboy/react-native-check-box)
-* [react-native-htmlview ^0.12.1](https://github.com/jsdf/react-native-htmlview)
+* [react-native-easy-toast ^1.2.0](https://github.com/crazycodeboy/react-native-easy-toast)
+* [react-native-check-box ^2.1.7](https://github.com/crazycodeboy/react-native-check-box)
+* [react-native-htmlview ^0.13.0](https://github.com/jsdf/react-native-htmlview)
 * [react-native-parallax-scroll-view ^0.21.3](https://github.com/i6mi6/react-native-parallax-scroll-view)
 * [react-native-sortable-listview ^0.2.8](https://github.com/deanmcpherson/react-native-sortable-listview)
-* [react-native-splash-screen ^3.0.9](https://github.com/crazycodeboy/react-native-splash-screen)
+* [react-native-splash-screen ^3.2.0](https://github.com/crazycodeboy/react-native-splash-screen)
 
 ## 运行调试
 
--- android/app/src/main/java/com/githubpopular/MainActivity.java
+++ android/app/src/main/java/com/githubpopular/MainActivity.java
@@ -1,6 +1,8 @@
 package com.githubpopular;
 
+import android.nfc.Tag;
 import android.os.Bundle; // here
+import android.util.Log;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -9,11 +11,50 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import org.devio.rn.splashscreen.SplashScreen; // here
 
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.PushAgent;
+
 public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this);  // here
         super.onCreate(savedInstanceState);
+
+        // 注意：如果您已经在AndroidManifest.xml中配置过appkey和channel值，可以调用此版本初始化函数。
+        UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, "d923513f02606b34c9aa1b6c4559e980");
+        // interval: 单位是毫秒，默认Session间隔时间是30秒
+        MobclickAgent.setSessionContinueMillis(30000);
+
+        //获取消息推送代理示例
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+        //注册推送服务，每次调用register方法都会回调该接口
+        mPushAgent.register(new IUmengRegisterCallback() {
+            @Override
+            public void onSuccess(String deviceToken) {
+                //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
+                Log.i("deviceToken", "注册成功：deviceToken：-------->  " + deviceToken);
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+                Log.e("deviceToken", "注册失败：-------->  " + "s:" + s + ",s1:" + s1);
+            }
+        });
+        PushAgent.getInstance(this).onAppStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
     }
 
     /**
